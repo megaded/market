4f46dbde727534a623dca390fc61519472c01518
Evgeniy <darkintel999@gmail.com>
2025-05-23 02:41:40 +0300

    ok

--- cmd/internal/handler/handler.go
+++ cmd/internal/handler/handler.go
@@ -190,6 +190,7 @@ func (h *Handler) Orders() func(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
 		if err = json.NewEncoder(w).Encode(orders); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			logger.Log.Info("failed to encode orders", zap.Error(err))
